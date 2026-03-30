f6c069eaa3ec1b071dcbe7a0804e3fc64d5f4069
Joljolionn <joljolionn@gmail.com>
2026-03-30 17:05:41 -0300

    feat: finalizar conexao entre front e back com docker

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-import dotenv from "dotenv";
-import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
+import dotenv from 'dotenv';
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 dotenv.config();
 
@@ -12,29 +12,57 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 export const config = {
-  nominatimApi: process.env.NOMINATIM_API,
-  orsApi: process.env.ORS_API,
-  orsKey: process.env.ORS_KEY,
-  country: process.env.COUNTRY || "br",
+    nominatimApi: process.env.NOMINATIM_API,
+    orsApi: process.env.ORS_API,
+    orsKey: process.env.ORS_KEY,
+    country: process.env.COUNTRY || 'br',
+    backendUrl: process.env.BACKEND_URL,
 };
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
-app.use(express.static(path.join(__dirname, "assets")));
-
-app.get("/request", (req, res) => {
-  res.sendFile(path.join(__dirname, "pages", "request-ride.html"));
-})
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'assets')));
 
 app.get("/env", (req, res) => {
   res.status(200).json(JSON.stringify(config))
 })
 
-app.listen(PORT, () => {
-  console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
-  console.log(`🌍 Configurado para o país: ${config.country}`);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
+
+app.get('/request', (req, res) => {
+    res.sendFile(path.join(__dirname, 'pages', 'request-ride.html'));
+});
+
+app.get('/account', (req, res) => {
+    res.sendFile(path.join(__dirname, 'pages', 'account.html'));
+});
+
+app.get('/signup', (req, res) => {
+    res.sendFile(path.join(__dirname, 'pages', 'signup.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
+    console.log(`🌍 Configurado para o país: ${config.country}`);
+});
+
+// Função para demonstrar a conexão funcional entre backend e frontend
+async function testLogin() {
+    const response = await fetch(config.backendUrl + '/passenger/login', {
+        headers: { 'Content-Type': 'application/json' },
+        method: 'POST',
+        body: JSON.stringify({
+            email: 'Luzinete@sumus.com',
+            password: 'senha321',
+        }),
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+}
 
 // TODO: Add default values if dotenv variables is null
 // Example: foo: process.env.FOO || null
-
